9f12c196112685c3f93bee7036983bfb1b73f9de
Carolina Grecia Inturias Santander <carolina2_insan@hotmail.es>
2022-07-03 10:13:06 -0400

    Avance en el formulario Marca.

--- Almacen/Parametros/frmMarca.cs
+++ Almacen/Parametros/frmMarca.cs
@@ -14,5 +14,10 @@ namespace Taller.Almacen.Parametros
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cerrar")
+        }
     }
 }
